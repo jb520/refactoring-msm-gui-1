2f9374c59b273a4aa1f99292cbc71a60f291c3a8
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-15 05:09:36 +0000

    refactor directors, movies, actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,22 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def title_with_year
+    return "#{self.title} (#{self.year})"
+  end
+
+  def director
+    d_id = self.director_id
+    the_director = Director.where({ :id => d_id }).at(0)
+    return the_director
+    
+  end
+
+  def characters
+    my_id = self.id
+    the_characters = Character.where({:movie_id => my_id})
+
+    return the_characters
+  end
+
 end
